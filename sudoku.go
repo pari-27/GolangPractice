1abf49c5a39956d84e0c76a522a8add7b36ebe93
paramita majumdar <parimajumdar2012@gmail.com>
2020-08-24 22:45:47 +0530

    core logic completed

--- sudoku.go
+++ sudoku.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
 	"strconv"
 	"strings"
 )
@@ -13,8 +14,10 @@ type Cell struct {
 	y int
 }
 
+var ans_grid map[Cell]int = make(map[Cell]int)
+
 func main() {
-	gridSize := 4 // 4 smaller grid of 2*2
+	gridSize := 9 // 4 smaller grid of 2*2
 	// a map for each cell location as key and the value of the cell
 	sudokuGrid := map[Cell]int{}
 
@@ -22,21 +25,96 @@ func main() {
 	for i := 0; i < gridSize; i++ {
 		for j := 0; j < gridSize; j++ {
 			sudokuGrid[Cell{i, j}] = 0
-			fmt.Println(i, j)
+			//fmt.Println(i, j)
 		}
 	}
-
+	rand.Seed(123456789123456789)
 	//change it to something dynamic
-	fmt.Println(safe_grid(sudokuGrid, 0, 1, 1))
-	fmt.Println(safe_grid(sudokuGrid, 0, 3, 4))
-	fmt.Println(safe_grid(sudokuGrid, 1, 0, 2))
-	fmt.Println(safe_grid(sudokuGrid, 1, 2, 1))
-	fmt.Println(safe_grid(sudokuGrid, 2, 0, 3))
-	fmt.Println(safe_grid(sudokuGrid, 2, 2, 2))
-	fmt.Println(safe_grid(sudokuGrid, 3, 1, 4))
-	fmt.Println(safe_grid(sudokuGrid, 3, 3, 3))
-
+	generate_grid(sudokuGrid)
+	get_ansGrid(sudokuGrid)
+	fmt.Println("---------------------------------")
 	render_grid(sudokuGrid)
+
+}
+
+func generate_grid(sudokuGrid map[Cell]int) bool {
+	gridSize := int(math.Sqrt(float64(len(sudokuGrid))))
+	count := 0
+	for count < 30 {
+		row := rand.Intn(gridSize)
+		col := rand.Intn(gridSize)
+		number := rand.Intn(gridSize) + 1
+		//fmt.Println(row, col, number)
+		if safe_grid(sudokuGrid, row, col, number) {
+			sudokuGrid[Cell{row, col}] = number
+			//render_grid(sudokuGrid)
+			if fitGrid(sudokuGrid) {
+
+				//fmt.Println(count)
+				count = count + 1
+			} else {
+				sudokuGrid[Cell{row, col}] = 0
+			}
+		}
+	}
+	return true
+}
+
+func getUnassignedLocation(sudokuGrid map[Cell]int) Cell {
+	gridSize := int(math.Sqrt(float64(len(sudokuGrid))))
+	for i := 0; i < gridSize; i++ {
+		for j := 0; j < gridSize; j++ {
+			if sudokuGrid[Cell{i, j}] == 0 {
+				return Cell{i, j}
+			}
+		}
+
+	}
+	return Cell{-1, -1}
+
+}
+func get_testgrid(sudokuGrid map[Cell]int) map[Cell]int {
+	test_grid := make(map[Cell]int)
+	for i, v := range sudokuGrid {
+		test_grid[i] = v
+
+	}
+	return test_grid
+}
+func get_ansGrid(sudukoGrid map[Cell]int) {
+
+	if fitGrid(sudukoGrid) {
+		render_grid(ans_grid)
+	}
+}
+func fitGrid(sudokuGrid map[Cell]int) bool {
+
+	test_grid := get_testgrid(sudokuGrid)
+
+	unassignedLocation := getUnassignedLocation(test_grid)
+	row, col := unassignedLocation.x, unassignedLocation.y
+	if row == -1 && col == -1 {
+		ans_grid = get_testgrid(test_grid)
+		return true
+	}
+	var check bool
+	gridSize := int(math.Sqrt(float64(len(sudokuGrid))))
+
+	for k := 1; k <= gridSize; k++ {
+
+		if safe_grid(test_grid, row, col, k) {
+			test_grid[Cell{row, col}] = k
+			check = fitGrid(test_grid)
+			if check == true {
+				return true
+			}
+
+		}
+		test_grid[Cell{row, col}] = 0
+
+	}
+
+	return false
 
 }
 
@@ -54,10 +132,10 @@ func safe_grid(sudokuGrid map[Cell]int, p int, q int, counter int) bool {
 
 	//check for the same integer in the row and column
 	for i := 0; i < gridSize; i++ {
-		if sudokuGrid[Cell{p, i}] == counter {
+		if i != q && sudokuGrid[Cell{p, i}] == counter {
 			row_flag = false
 		}
-		if sudokuGrid[Cell{i, q}] == counter {
+		if i != p && sudokuGrid[Cell{i, q}] == counter {
 			column_flag = false
 		}
 
@@ -66,7 +144,7 @@ func safe_grid(sudokuGrid map[Cell]int, p int, q int, counter int) bool {
 	row_index := int(p / no_of_block)
 	column_index := int(q / no_of_block)
 	start_index := Cell{row_index * no_of_block, column_index * no_of_block}
-	end_index := Cell{(row_index * no_of_block) + 2, (column_index * no_of_block) + 2}
+	end_index := Cell{(row_index * no_of_block) + (no_of_block - 1), (column_index * no_of_block) + (no_of_block - 1)}
 
 	//check for the same integer the same box
 	for i := start_index.x; i <= end_index.x; i++ {
@@ -77,14 +155,14 @@ func safe_grid(sudokuGrid map[Cell]int, p int, q int, counter int) bool {
 
 		}
 	}
+	//fmt.Println(empty_flag, row_flag, column_flag, box_flag)
+	// render_grid(sudokuGrid)
 	//check for safe grid
 	if empty_flag && row_flag && column_flag && box_flag {
-		sudokuGrid[Cell{p, q}] = counter
 		return true
 	} else {
 		return false
 	}
-	//fmt.Println(empty_flag, row_flag, column_flag, box_flag)
 
 }
 
@@ -93,11 +171,14 @@ func render_grid(sudokuGrid map[Cell]int) {
 
 	for i := 0; i < gridSize; i++ {
 		for j := 0; j < gridSize; j++ {
-			str := strings.Join([]string{"[", strconv.Itoa(sudokuGrid[Cell{i, j}]), "]"}, "")
-			fmt.Printf("%s", str)
 
+			str := strings.Join([]string{"[", strconv.Itoa(sudokuGrid[Cell{i, j}]), "]"}, " ")
+			if (j+1)%3 == 0 && j+1 < gridSize {
+				str = strings.Join([]string{"[", strconv.Itoa(sudokuGrid[Cell{i, j}]), "]", "  |  "}, " ")
+			}
+			fmt.Printf("%s", str)
 		}
-		fmt.Println()
+		fmt.Println("\n")
 	}
 
 }
